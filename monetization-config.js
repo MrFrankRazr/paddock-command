4500ab9bc0e8558765b38ca0ab5f2430104a4cdd
MrFrankRazr <frankreyna@gmail.com>
2026-09-25 16:56:46 -0700

    Add files via upload

--- monetization-config.js
+++ monetization-config.js
@@ -1,8 +1,9 @@
+
 /* Paddock Command monetization configuration.
  * Only approved partner destinations should be enabled here.
  */
 window.PC_MONETIZATION_CONFIG = Object.freeze({
-  version: '2.5.0',
+  version: '2.5.1',
   ads: {
     enabled: false,
     provider: '',
@@ -16,6 +17,11 @@ window.PC_MONETIZATION_CONFIG = Object.freeze({
         href: 'https://expedia.com/affiliates/expedia-home.IfOEwW0',
         merchant: 'Expedia',
         campaign: 'race-weekend-travel'
+      },
+      booksCollectibles: {
+        href: 'https://fanatics.93n6tx.net/1GdYZz',
+        merchant: 'Fanatics',
+        campaign: 'f1-fan-library'
       }
     }
   },
